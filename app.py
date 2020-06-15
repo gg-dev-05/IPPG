62854926ef436195b226c3988366ba3773e17d09
= <garvitgalgat@gmail.com>
2020-06-15 14:19:30 +0530

    app.py is working correctly

--- app.py
+++ app.py
@@ -28,10 +28,11 @@ def handle_request():
 def func():
 	req = rq.get_json()
 	username = req['username']
+	username1 = "{}\n".format(username)
 	isSaved = False
 
 	with open('users.txt') as f:
-		result = re.findall('\\b'+username+'\\b', f.read(), flags=re.IGNORECASE)
+		result = re.findall('\\b'+username1+'\\b', f.read(), flags=re.IGNORECASE)
 		if(len(result) > 0):
 			isSaved = True
 
